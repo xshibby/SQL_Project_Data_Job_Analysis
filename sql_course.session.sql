620714e89335ade397fe8f8546ae7e0f121a5cd1
IGOR_KLIMACKI <igorklimacki@gmail.com>
2026-03-29 18:26:22 +0100

    push example

--- sql_course.session.sql
+++ sql_course.session.sql
@@ -1,21 +1,3 @@
-WITH number_of_postings AS ( 
-  SELECT
-    company_id,
-    count(*) AS total_jobs
-  FROM
-    job_postings_fact
-  GROUP BY
-    company_id
-)
-
-SELECT
-  name AS company_name,
-  CASE
-    WHEN total_jobs > 10 THEN 'small'
-    WHEN total_jobs BETWEEN 10 AND 50 THEN 'medium'
-    ELSE 'large'
-  END AS company_size
-FROM
-  company_dim
-LEFT JOIN number_of_postings ON number_of_postings.company_id = company_dim.company_id
-
+SELECT *
+from
+job_postings_fact
