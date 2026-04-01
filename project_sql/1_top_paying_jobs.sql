ae250c20dabc007e65914dd5693cfb4e501663f4
IGOR_KLIMACKI <igorklimacki@gmail.com>
2026-04-01 19:44:23 +0100

    Completed first two queries, and analyzed the data produced.

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,26 @@
+/*
+Question: What are the top-paying data analyst jobs:
+- Identify the top 10 highest-paying Data Analyst roles that are available locally
+- Focues on job postings with specified salaries (remove nulls).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment opportunities
+*/
+
+SELECT
+  job_id,
+  job_title,
+  company_dim.name AS company_name,
+  job_location,
+  job_schedule_type,
+  salary_year_avg,
+  job_posted_date
+FROM
+  job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+  job_title_short = 'Data Analyst' AND
+  job_location LIKE '%UK' AND
+  salary_year_avg IS NOT NULL
+ORDER BY
+  salary_year_avg DESC
+LIMIT
+  10
--- sql_course.session.sql
+++ sql_course.session.sql
@@ -1,3 +1,5 @@
 SELECT *
 from
 job_postings_fact
+WHERE
+  job_id = '478395'
